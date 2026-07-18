c38b3e794f2bcd9424fe00592d6105c3c11b2739
Imam Khan <imamuddinkhan1700@gmail.com>
2026-07-18 04:28:01 -0700

    Initial Spring Boot project setup

---
+++ ecommerce-baceknd/src/main/java/ecommerce_baceknd/EcommerceBacekndApplication.java
@@ -0,0 +1,13 @@
+package ecommerce_baceknd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBacekndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBacekndApplication.class, args);
+	}
+
+}
